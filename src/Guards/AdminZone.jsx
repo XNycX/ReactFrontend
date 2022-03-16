2ee79c06cccce008de21ece8d4d94eaf3be3f52e
XNycX <nycx2009@hotmail.com>
2022-03-16 14:11:24 +0100

    AdminZone error fixed

--- src/Guards/AdminZone.jsx
+++ src/Guards/AdminZone.jsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 
 const AdminZone = ({ children }) => {
     const user = JSON.parse(localStorage.getItem("redux_localstorage_simple_credentials"));
-  return user?.user.role === 'admin' ? children : <Navigate to="/" />;
+  return user?.user?.role === 'admin' ? children : <Navigate to="/" />;
 };
 
 export default AdminZone;
