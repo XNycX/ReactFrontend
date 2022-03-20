a5380f92477cf30a1d8815da4933fdcc4e011888
XNycX <nycx2009@hotmail.com>
2022-03-20 14:43:33 +0100

    Footer text updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <div className="footer">
-           <div className="footerLetter">GeeksHubs Academy - 2022</div>
+           <div className="footerLetter">Cristian Santamaria Rodriguez - 2022</div>
         </div>
     )
 };
