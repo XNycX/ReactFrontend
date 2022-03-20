3046627ec2f318cc8067a5749433d028b2683d31
XNycX <nycx2009@hotmail.com>
2022-03-20 18:42:26 +0100

    spinner updated

--- src/Containers/Movies/Movies.jsx
+++ src/Containers/Movies/Movies.jsx
@@ -4,14 +4,14 @@ import { getMovies } from "../../redux/actions/movie";
 import Movie from "../../Components/Movie/Movie";
 import { Loader } from "@mantine/core";
 import { connect } from "react-redux";
-
+import loader from '../../images/loader.gif'
 const Movies = (props) => {
   useEffect(() => {
     getMovies();
   }, []);
-  if (!props.movies) return <Loader />;
   return (
     <div className="designRooster">
+    {props.movies?.length < 0 ? <img src={loader}  /> : ''} 
       <Movie />
     </div>
   );
