3cbc522de37cc6330972c9f51080b8b6a2b358fe
“Cristian” <nycx2009@hotmail.com>
2022-03-21 08:59:13 +0100

    Updated naming on utiles.js

--- src/utiles.js
+++ src/utiles.js
@@ -3,21 +3,21 @@ export const checkError = (type,value) => {
         case 'email' :
             if (! /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(value) ) {
                 
-                return "Introduce un e-mail válido";
+                return "Enter a valid email";
             }else{
                 return "ok";
             };     
         case 'name': 
 
             if (! /[a-z]/gi.test(value) ) {
-                return "Introduce un nombre válido";
+                return "Enter a valid name";
             }else{
                 return "ok";
             };
         case 'telephone':
 
             if (! /[\d()+-]/g.test(value) ) {
-                return "Introduce un telefono válido";
+                return "Enter a valid telephone";
             }else{
                 return "ok";
             };
